aeb1c0a483a1edfddfd1d9325bbe4c18eba12090
DeiAye <72456002+DeiAye@users.noreply.github.com>
2020-10-23 11:33:50 -0500

    updated array and main class

--- src/com/floreo/bbah/Bot.java
+++ src/com/floreo/bbah/Bot.java
@@ -6,14 +6,58 @@ import com.floreo.bbah.network.Slack;
 import com.floreo.bbah.network.responses.*;
 
 import java.util.List;
-
+import java.util.Random;
 public class Bot {
 
     // TODO: implement your bot logic!
 
-    public Bot() {
 
-    }
+        public String inspirationalQuotes() {
+            String[] text = new String[16];
+            text[0] = "Genius is one percent inspiration and ninety-nine percent perspiration.";
+            text[1] = "You can observe a lot just by watching.";
+            text[2] = "A house divided against itself cannot stand.";
+            text[3] = "Difficulties increase the nearer we get to the goal.";
+            text[4] = "Fate is in your hands and no one elses";
+            text[5] = "Be the chief but never the lord.";
+            text[6] = "Nothing happens unless first we dream.";
+            text[7] = "Well begun is half done.";
+            text[8] = "Life is a learning experience, only if you learn.";
+            text[9] = "Self-complacency is fatal to progress.";
+            text[10] = "Peace comes from within. Do not seek it without.";
+            text[11] = "What you give is what you get.";
+            text[12] = "We can only learn to love by loving.";
+            text[13] = "Life is change. Growth is optional. Choose wisely.";
+            text[14] = "You'll see it when you believe it.";
+            text[15] = "Today is the tomorrow we worried about yesterday.";
+
+
+            String[] author = new String[16];
+            author[0] = "Benjamin Franklin";
+            author[1] = "Yogi Berra";
+            author[2] = "Abraham Lincoln";
+            author[3] = "Johann Wolfgang van Goethe";
+            author[4] = "Byron Pulsifer";
+            author[5] = "Lao Tzu";
+            author[6] = "Carl Sandburg";
+            author[7] = "Aristotle";
+            author[8] = "Yogi Berra";
+            author[9] = "Margaret Sangster";
+            author[10] = "Buddha";
+            author[11] = "Byron Pulsifer";
+            author[12] = "Iris Murdoch";
+            author[13] = "Karen Clark";
+            author[14] = "Wayne Dyer";
+            author[15] = "Unknown";
+
+
+
+            Random quotes3 = new Random();
+            int inspiration = quotes3.nextInt(16 - 1);
+            Slack.sendMessage(text[inspiration] + author[inspiration]);
+            return text[inspiration];
+        }
+
 
     /**
      * Sample method: tests the Slack API. Prints a message indicating success or failure.
--- src/com/floreo/bbah/Main.java
+++ src/com/floreo/bbah/Main.java
@@ -1,25 +1,27 @@
 package com.floreo.bbah;
-
+import com.floreo.bbah.model.Channel;
+import com.floreo.bbah.model.Message;
 import com.floreo.bbah.network.Slack;
+import com.floreo.bbah.network.responses.*;
+import java.util.Random;
+import java.util.List;
 
 public class Main {
-
-    public static void main(String[] args) {
-
+public static void main(String[] args) {
         Bot myBot = new Bot();
-
         myBot.testApi();
-
         myBot.listChannels();
-
-        myBot.listMessages(Slack.BOTS_CHANNEL_ID);
-
-
+        //myBot.listMessages(Slack.BOTS_CHANNEL_ID);
         // Post "Hello, world!" to the #bots channel
-        //myBot.sendMessage("Hello, world!");
-
+        //myBot.sendMessageToBotsChannel("Hello, world!");
+        myBot.sendMessageToBotsChannel("Here is your inspirational quote:");
+        myBot.inspirationalQuotes();
         // Post a pineapple photo to the #bots channel
-        //myBot.sendMessage("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
+        //myBot.sendMessageToBotsChannel("http://weknowyourdreams.com/images/pineapple/pineapple-07.jpg");
+
+        }
+
+
 
     }
-}
+
